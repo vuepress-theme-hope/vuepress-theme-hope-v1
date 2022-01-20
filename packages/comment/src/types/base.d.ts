ce20dff6b44f98ac134602e0137dd70f6f8565b4
Mr.Hope <zhangbowang1998@gmail.com>
2022-01-20 13:30:53 +0800

    refactor(comment): improve types

--- packages/comment/src/types/base.d.ts
+++ packages/comment/src/types/base.d.ts
@@ -3,13 +3,6 @@ import type { PageInfoLocaleData, PageInfo } from "./pageInfo";
 
 export interface BaseCommentOptions {
   /**
-   * 评论服务
-   *
-   * Comment Service
-   */
-  type: "valine" | "vssue" | "disable";
-
-  /**
    * 默认作者
    *
    * Default author
@@ -17,43 +10,44 @@ export interface BaseCommentOptions {
   author?: string;
 
   /**
-   * 文章信息配置
-   *
    * Page Info display configuration
    *
-   * @see https://vuepress-theme-hope.github.io/comment/zh/config/#pageinfo (zh)
-   * @see https://vuepress-theme-hope.github.io/comment/config/#pageinfo (en)
+   * @see https://vuepress-theme-hope.github.io/comment/config/#pageinfo
+   *
+   * 文章信息配置
+   *
+   * @see https://vuepress-theme-hope.github.io/comment/zh/config/#pageinfo
    *
    * @default ['Author', 'Visitor', 'Time', 'Category', 'Tag', 'ReadTime']
    */
   pageInfo?: PageInfo[] | false;
 
   /**
-   * 是否默认启用评论
-   *
    * Whether enable comment by default
+   *
+   * 是否默认启用评论
    *
    * @default true
    */
   comment?: boolean;
 
   /**
-   * 每分钟阅读字数
-   *
    * Reading speed of words per minute
+   *
+   * 每分钟阅读字数
    *
    * @default 300
    */
   wordPerminute?: number;
 
   /**
-   * 进行 DOM 操作的延时，单位 ms
-   *
-   * 如果你使用的主题有切换动画，建议配置此选项为 `切换动画时长 + 200`
-   *
    * The delay of dom operation, in ms
    *
    * If the theme you are using has a switching animation, it is recommended to configure this option to `Switch animation duration + 200`
+   *
+   * 进行 DOM 操作的延时，单位 ms
+   *
+   * 如果你使用的主题有切换动画，建议配置此选项为 `切换动画时长 + 200`
    *
    * @default 500
    */
@@ -61,6 +55,8 @@ export interface BaseCommentOptions {
 
   /**
    * Locales config for pageInfo
+   *
+   * 页面信息的国际化配置
    */
   pageInfoLocales?: LocaleConfig<PageInfoLocaleData>;
 }
--- packages/comment/src/types/pageInfo.d.ts
+++ packages/comment/src/types/pageInfo.d.ts
@@ -1,30 +1,69 @@
 import type { ConvertLocaleConfig } from "@mr-hope/vuepress-shared";
 
 export interface PageInfoLocaleData {
-  /** 作者 */
+  /**
+   * Author label text
+   *
+   * 作者文字
+   */
   author: string;
-  /** 写作日期 */
+
+  /**
+   * Writing date label text
+   *
+   * 写作日期文字
+   */
   date: string;
-  /** 原创文字 */
+
+  /**
+   * Label text marked as original
+   *
+   * 标记原创的文字
+   */
   origin: string;
-  /** 访问量 */
+
+  /**
+   * Page views label text
+   *
+   * 访问量文字
+   */
   views: string;
-  /** 标签文字 */
+
+  /**
+   * Tag label text
+   *
+   * 标签文字
+   */
   tag: string;
-  /** 分类文字 */
+
+  /**
+   * Category label text
+   *
+   * 分类文字
+   */
   category: string;
-  /** 阅读时间 */
+
+  /**
+   * Expect reading time label text
+   *
+   * 期望阅读时间文字
+   */
   readingTime: string;
-  /** 文章字数 */
+
+  /**
+   * Words label Text
+   *
+   * 文章字数
+   */
   words: string;
 }
 
 export type PageInfoLocaleConfig = ConvertLocaleConfig<PageInfoLocaleData>;
 
 /**
- * 页面信息类型
+ * Type of page infomation
  *
- * Type of page information
+ * 页面信息类型
  */
 export type PageInfo =
   | "author"
