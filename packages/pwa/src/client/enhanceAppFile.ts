7bd347e086c857312439c8d3ecc40b36d8871723
Mr.Hope <mister-hope@outlook.com>
2022-06-02 23:03:03 +0800

    fix(pwa): fix typos

--- packages/pwa/src/client/enhanceAppFile.ts
+++ packages/pwa/src/client/enhanceAppFile.ts
@@ -1,6 +1,6 @@
 import PWAInstall from "./components/PWAInstall.vue";
-import SWHintPopup from "./SWHintPopup.vue";
-import SWUpdatePopup from "./SWUpdatePopup.vue";
+import SWHintPopup from "./components/SWHintPopup.vue";
+import SWUpdatePopup from "./components/SWUpdatePopup.vue";
 
 import type { EnhanceApp } from "@mr-hope/vuepress-types";
 
