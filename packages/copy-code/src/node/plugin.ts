ab4d06036afa7ac5557207fbd8aede7e4deae71e
Mr.Hope <mister-hope@outlook.com>
2022-06-02 14:46:27 +0800

    feat(copy-code): update style

---
+++ packages/copy-code/src/node/plugin.ts
@@ -0,0 +1,27 @@
+import { getLocales } from "@mr-hope/vuepress-shared";
+import { resolve } from "path";
+import { copyCodeLocales } from "./locales";
+
+import type { Plugin } from "@mr-hope/vuepress-types";
+import type { CopyCodeOptions } from "../types";
+
+export const copyCodePlugin: Plugin<CopyCodeOptions> = (options, context) => {
+  const userCopyCodeLocales = getLocales(
+    context,
+    copyCodeLocales,
+    options.locales
+  );
+
+  delete options.locales;
+
+  return {
+    name: "copy-code",
+
+    define: (): Record<string, unknown> => ({
+      CODE_COPY_OPIONS: options,
+      CODE_COPY_LOCALES: userCopyCodeLocales,
+    }),
+
+    clientRootMixin: resolve(__dirname, "../client/clientRootMixin.js"),
+  };
+};
