1f1db10cec307c860b7139c39556b64aeb5654ef
Mr.Hope <mister-hope@outlook.com>
2022-06-03 23:07:46 +0800

    refactor: update active-hash plugin

--- packages/active-hash/src/node/index.ts
+++ packages/active-hash/src/node/index.ts
@@ -1,21 +1,3 @@
-import { resolve } from "path";
-import type { Plugin } from "vuepress-typings";
-
-import type { ActiveHashOptions } from "../types";
-
-const activeHashPlugin: Plugin<ActiveHashOptions> = (options) => ({
-  name: "active-hash",
-
-  define: (): Record<string, unknown> => ({
-    ACTIVE_HASH_ACTIVE_SELECTOR: options.activeLinkSelecter || ".sidebar-link",
-    ACTIVE_HASH_CONTAINER_SELECTOR:
-      options.containerSelecter || ".theme-default-content",
-    ACTIVE_HASH_HEADER_SELECTOR:
-      options.headerAnchorSelector || ".header-anchor",
-    ACTIVE_HASH_OFFSET: options.offset || 0,
-  }),
-
-  clientRootMixin: resolve(__dirname, "../client/rootMixin.js"),
-});
+import { activeHashPlugin } from "./plugin";
 
 export = activeHashPlugin;
---
+++ packages/active-hash/src/node/plugin.ts
@@ -0,0 +1,19 @@
+import { resolve } from "path";
+import type { Plugin } from "vuepress-typings";
+
+import type { ActiveHashOptions } from "../types";
+
+export const activeHashPlugin: Plugin<ActiveHashOptions> = (options) => ({
+  name: "vuepress-plugin-active-hash",
+
+  define: (): Record<string, unknown> => ({
+    ACTIVE_HASH_ACTIVE_SELECTOR: options.activeLinkSelecter || ".sidebar-link",
+    ACTIVE_HASH_CONTAINER_SELECTOR:
+      options.containerSelecter || ".theme-default-content",
+    ACTIVE_HASH_HEADER_SELECTOR:
+      options.headerAnchorSelector || ".header-anchor",
+    ACTIVE_HASH_OFFSET: options.offset || 0,
+  }),
+
+  clientRootMixin: resolve(__dirname, "../client/rootMixin.js"),
+});
